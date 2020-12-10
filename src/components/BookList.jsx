edd716f4b86d93c5765f324fb1698b0c8db16f94
jonna ru <jonna.ru@icloud.com>
2020-12-10 19:35:17 +0100

    useState hook books + mapping books

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const BookList = () => {
   const [page, setPage] = useState(1);
+  const [books, setBooks] = useState([]);
 
   const URL = `https://books-express.herokuapp.com/books?page=${page}`;
 
@@ -11,12 +12,20 @@ export const BookList = () => {
         return res.json();
       })
       .then((data) => {
-        console.log("data", data);
+        setBooks(data);
       });
   }, [URL]);
 
+  console.log(books);
+
   return (
     <div>
+      {books.map((book) => (
+        <div>
+          <p>{book.title}</p>
+          <p>{book.authors}</p>
+        </div>
+      ))}
       <button onClick={() => setPage((prev) => prev + 1)}>next</button>
       {page > 1 && (
         <button onClick={() => setPage((prev) => prev - 1)}>back</button>
